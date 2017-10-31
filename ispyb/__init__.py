e273874eff5a8ffc7dee51cc8cb2c42affef9d40
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-31 14:08:25 +0000

    close repository

--- ispyb/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-__version__ = "0.16"
-
-import ispyb.legacy.common.driver
-
-legacy_get_driver = ispyb.legacy.common.driver.get_driver
-legacy_Backend = ispyb.legacy.common.driver.Backend
